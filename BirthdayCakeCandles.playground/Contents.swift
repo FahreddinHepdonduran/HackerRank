f1b5c92be6f3145e6cfecab3f7373ae1d92486a4
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2024-05-25 23:16:26 +0300

    birthday cake candles problem

---
+++ BirthdayCakeCandles.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+func birthdayCakeCandles(candles: [Int]) -> Int {
+    let sortedCandles = candles.sorted { $0 < $1}
+    guard let highestNumber = sortedCandles.last else { return 0 }
+    let filteredCandles = sortedCandles.filter { $0 == highestNumber }
+    return filteredCandles.count
+}
