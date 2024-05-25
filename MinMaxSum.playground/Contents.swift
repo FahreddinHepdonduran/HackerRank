4ce74f96e29c272526c37054d3ae8c4cf626d750
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2024-05-25 20:20:41 +0300

    min max sum problem

---
+++ MinMaxSum.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+func miniMaxSum(arr: [Int]) -> Void {
+    // TODO: fix here - array my be less than four emelent
+    let sortedArray = arr.sorted { $0 < $1 }
+    let firstFourElement = Array(sortedArray.prefix(4))
+    let lastFourElement = Array(sortedArray.suffix(4))
+    let sumOfFirstFourElement = firstFourElement.reduce(0, { $0 + $1 })
+    let sumOfLastFourElement = lastFourElement.reduce(0, { $0 + $1 })
+    print("\(sumOfFirstFourElement)  \(sumOfLastFourElement)")
+}
+
+miniMaxSum(arr: [1, 3, 5, 7, 9])
